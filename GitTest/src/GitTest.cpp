883c529f07c30685c008e1780a96a8a8939d3693
CYChen60 <cychen60@gmail.com>
2015-11-05 09:07:56 +0800

    Update GitTest.cpp

--- GitTest/src/GitTest.cpp
+++ GitTest/src/GitTest.cpp
@@ -10,6 +10,6 @@
 using namespace std;
 
 int main() {
-	cout << "!!!Hello World!!! mod 5" << endl; // prints !!!Hello World!!!
+	cout << "!!!Hello World!!! GitHub" << endl; // prints !!!Hello World!!!
 	return 0;
 }
